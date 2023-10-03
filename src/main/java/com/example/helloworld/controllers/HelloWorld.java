a2d605ac7883cb69f0175167688a4270e69040a7
rushi52 <rushi.jammana@gmail.com>
2023-10-03 11:20:41 +0530

    Added helloworld controller

---
+++ src/main/java/com/example/helloworld/controllers/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.example.helloworld.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class HelloWorld {
+    @GetMapping
+    private List helloWorld()
+    {
+        return List.of("Hello","World","!!");
+
+    }
+
+}
