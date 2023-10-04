e5d3f6e60d0c09ac58fb642ab7797cd9b2f9656d
rushi52 <rushi.jammana@gmail.com>
2023-10-04 11:15:02 +0530

    helloworld controller

--- src/main/java/com/example/helloworld/controllers/HelloWorld.java
+++ src/main/java/com/example/helloworld/controllers/HelloWorld.java
@@ -1,5 +1,7 @@
 package com.example.helloworld.controllers;
 
+import com.example.helloworld.services.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,10 +9,12 @@ import java.util.List;
 
 @RestController
 public class HelloWorld {
+    @Autowired
+    private HelloWorldService helloWorldService;
     @GetMapping
     private List helloWorld()
     {
-        return List.of("Hello","World 123");
+        return helloWorldService.printHelloWorld();
 
     }
 
